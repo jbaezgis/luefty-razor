01fd1b775c0317c2594da2909d6123efa2555506
Yoel Baez <jbaezgis@gmail.com>
2021-12-28 12:59:02 -0400

    Mostrando locations

---
+++ Luefty/Model/BlazorCountry.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Luefty.Model
+{
+	public class BlazorCountry
+	{
+		public int Id { get; set; }
+		public string? Name { get; set; }
+	}
+}
+
---
+++ Luefty/Model/BlazorLocation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Luefty.Model
+{
+	public class BlazorLocation
+	{
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool Active { get; set; }
+        public List<BlazorCountry>? BlazorCountries { get; set; }
+    }
+}
+
